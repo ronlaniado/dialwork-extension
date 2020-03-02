036a157bd92f5e0019b39ae5a101fb50dbc7f152
Ron <laniado.ron@gmail.com>
2020-03-01 19:58:59 -0500

    adding functionality to collect inputs

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,29 @@
-console.log("content script is alive!");
-let input = new Object();
+if (typeof input === "undefined") {
+	// Assigning to window.input creates the global
+	window.keys = new Array();
+	window.input = new Object();
+}
 
-$("input[name]").each(function() {
-	input[$(this).attr("name")] = $(this).val();
-});
+getInfo();
 
-console.log(input);
+function getInfo() {
+	$("input[name]").each(function() {
+		if ($(this).val() !== "") {
+			// Stores all non-empty inputs into an object
+			input[$(this).attr("name")] = $(this).val();
+		}
+	});
+	let currentUrl = window.location.href;
+	// current url of page will be used as the key for data object
 
-// Only collect inputs if it is not empty
+	chrome.storage.sync.set({ [currentUrl]: input }, () => {
+		console.log("Form has been saved to storage");
+	});
+	keys.push(currentUrl);
+	console.log(input);
+
+	// Now I need to have a message be sent to the background page, which will manage all the keys I need
+	chrome.runtime.sendMessage({ message: keys }, (response) => {
+		console.log(response.farewell);
+	});
+}
