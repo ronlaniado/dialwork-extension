96d308e17d59048f502f4e267f7e671084d714a2
Ron <laniado.ron@gmail.com>
2020-03-01 20:38:53 -0500

    removed uncessary console.logs

--- options.js
+++ options.js
@@ -1,10 +1,7 @@
 $(document).ready(() => {
-	console.log("options page is ready!");
-
 	chrome.runtime.sendMessage({ message: "keyRequest" }, (response) => {
 		//Sends a message to the background script telling it that options.html is active, and that we want the keys.
 		let keys = response.farewell;
-		console.log(keys);
 		getData(keys);
 	});
 });
@@ -18,24 +15,22 @@ const getData = (keys) => {
 
 const renderData = (result) => {
 	// Renders a collapsible list for each link that has key:value pairs inside of it
-	console.log("Rendering data...");
 	console.log(result);
 	let title = "";
 	let rows = "";
 	for (const url in result) {
-		title = "<h5 class='text-white text-center'>" + url + "</h5>";
+		title = "<h5 class='text-white text-center url'>" + url + "</h5>";
 		rows = "";
 		for (const value in result[url]) {
-			console.log(value + ":" + result[url][value]);
 			rows +=
 				"<div class='input-group input-group-sm d-flex flex-row my-2' id=" +
 				value +
-				`><input type='text' class='form-control font-weight-bold input-left' value=${value} id='hi'>` +
-				`</input><input type='text' class='form-control input-right' value=${result[url][value]}/ aria-describedby='cancel'>` +
+				`><input type='text' class='form-control font-weight-bold input-left' value="${value}" id='hi'>` +
+				`</input><input type='text' class='form-control input-right' value="${result[url][value]}"/ aria-describedby='cancel'>` +
 				"</input><div class='input-group-append cancel-button'><span class='input-group-text' id='cancel'>X</span></div></div>";
 		}
 		const saveButton =
-			"<button class='btn btn-warning center save-button' type='button' onclick='e.preventDefault()' style='display:none'>Save changes</button>";
+			"<button class='btn btn-warning center save-button' type='button' style='display:none'>Save changes</button>";
 		$(".container").append(
 			"<div class='bg-info rounded-sm py-5 px-3'>" +
 				title +
@@ -48,19 +43,16 @@ const renderData = (result) => {
 	}
 
 	bindOnBlur();
+	// bind on blur adds the detection to check for empty inputs in order to remove them
+
 	bindCancel();
 	bindButtonClick();
-	saveChanges();
-	// bind on blur adds the detection to check for empty inputs in order to remove them
 };
 
 const bindOnBlur = () => {
 	$("#input-group :input").blur((e) => {
-		console.log("focused removed");
 		if (e.target.value.length === 0) {
-			console.log("detected an empty input!");
 			const parentId = e.target.parentNode.id;
-			console.log($(`#${$.escapeSelector(parentId)}`));
 			$(`#${$.escapeSelector(parentId)}`).remove();
 			$(".save-button").css("display", "block");
 		} else {
@@ -74,16 +66,16 @@ const bindCancel = () => {
 	$(".cancel-button").click((e) => {
 		console.log("cancel button clicked!");
 		const parentId = e.target.parentNode.parentNode.id;
-		console.log($(`#${$.escapeSelector(parentId)}`));
 		$(`#${$.escapeSelector(parentId)}`).remove();
 		$(".save-button").css("display", "block");
 	});
 };
 
 const bindButtonClick = () => {
-	$(".save-button").click(() => {
+	$(".save-button").click((e) => {
 		console.log("detected button click");
+		const parentDiv = $.escapeSelector(e.target.parentNode.parentNode.id);
+		let url = $(parentDiv).find("#url");
+		console.log(url);
 	});
 };
-
-const saveChanges = () => {};
