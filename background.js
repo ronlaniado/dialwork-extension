161b7db2a493560df1717d4979e915daf6bf0bb0
Ron <laniado.ron@gmail.com>
2020-03-01 19:58:40 -0500

    background script handles chrome storage

--- background.js
+++ background.js
@@ -1,3 +1,30 @@
 chrome.runtime.onInstalled.addListener(function() {
 	console.log("Background script has been installed successfully.");
 });
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+	for (let key in changes) {
+		let storageChange = changes[key];
+		console.log(
+			'Storage key "%s" in namespace "%s" changed. ' + 'Old value was "%s", new value is "%s".',
+			key,
+			namespace,
+			storageChange.oldValue,
+			storageChange.newValue
+		);
+		console.log(changes);
+	}
+});
+
+let keys;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	// Gets the keys to the data stored by the extension
+	if (request.message === "keyRequest") {
+		sendResponse({ farewell: keys });
+	} else if (Array.isArray(request.message)) {
+		keys = request.message;
+		console.log("keys: " + keys);
+		sendResponse({ farewell: "The url keys have been received" });
+	}
+});
