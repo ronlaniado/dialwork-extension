c61beb7ca2628e52c8ee40576e3f98ae1244abb3
Ron <laniado.ron@gmail.com>
2020-03-02 15:29:46 -0500

    class and id changes

--- options.js
+++ options.js
@@ -19,20 +19,20 @@ const renderData = (result) => {
 	let title = "";
 	let rows = "";
 	for (const url in result) {
-		title = "<h5 class='text-white text-center url'>" + url + "</h5>";
+		title = `<h5 class='text-white text-center' id="${url}">` + url + "</h5>";
 		rows = "";
 		for (const value in result[url]) {
 			rows +=
 				"<div class='input-group input-group-sm d-flex flex-row my-2' id=" +
 				value +
-				`><input type='text' class='form-control font-weight-bold input-left' value="${value}" id='hi'>` +
-				`</input><input type='text' class='form-control input-right' value="${result[url][value]}"/ aria-describedby='cancel'>` +
+				`><input type='text' class='form-control font-weight-bold input-left key' value="${value}">` +
+				`</input><input type='text' class='form-control input-right value' value="${result[url][value]}"/ aria-describedby='cancel'>` +
 				"</input><div class='input-group-append cancel-button'><span class='input-group-text' id='cancel'>X</span></div></div>";
 		}
 		const saveButton =
 			"<button class='btn btn-warning center save-button' type='button' style='display:none'>Save changes</button>";
 		$(".container").append(
-			"<div class='bg-info rounded-sm py-5 px-3'>" +
+			"<div class='bg-info rounded-sm py-5 px-3 data-container'>" +
 				title +
 				"<div>" +
 				rows +
@@ -56,7 +56,6 @@ const bindOnBlur = () => {
 			$(`#${$.escapeSelector(parentId)}`).remove();
 			$(".save-button").css("display", "block");
 		} else {
-			let newValue = e.target.value;
 			chrome.runtime.sync.set({});
 		}
 	});
@@ -66,6 +65,7 @@ const bindCancel = () => {
 	$(".cancel-button").click((e) => {
 		console.log("cancel button clicked!");
 		const parentId = e.target.parentNode.parentNode.id;
+		console.log(parentId);
 		$(`#${$.escapeSelector(parentId)}`).remove();
 		$(".save-button").css("display", "block");
 	});
@@ -73,9 +73,28 @@ const bindCancel = () => {
 
 const bindButtonClick = () => {
 	$(".save-button").click((e) => {
-		console.log("detected button click");
-		const parentDiv = $.escapeSelector(e.target.parentNode.parentNode.id);
-		let url = $(parentDiv).find("#url");
-		console.log(url);
+		let dataString = "";
+		let data = {};
+		console.log("button clicked!");
+		$(".data-container").each(() => {
+			dataString;
+			let url = $("h5").text();
+			data[url] = {};
+			$(".input-group").each((i, val) => {
+				let key = $(val)
+					.find("input:nth-child(1)")
+					.val();
+				let value = $(val)
+					.find("input:nth-child(2)")
+					.val();
+				data[url][key] = value;
+			});
+			console.log(data);
+		});
+
+		// search through and find all inputs, then push them into a javascript array. Every other input should be paired as a key:value
+		// set the title to the const url
+		// make title the outmost key, with the values being a nested object
+		// nested object will have the key value pairs as mentioned above
 	});
 };
