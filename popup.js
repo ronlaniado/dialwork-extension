304bd3e6629e5a35171920c07aa85f46e4f42388
Ron <laniado.ron@gmail.com>
2020-03-07 14:21:36 -0500

    popup only renders default profile

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 	chrome.runtime.sendMessage({ message: "keyRequest" }, (response) => {
 		//Sends a message to the background script telling it that options.html is active, and that we want the keys.
 		let keys = response.farewell;
-		getData(keys);
+		renderDefaultProfile();
 	});
 	$("#collectButton").on("click", () => {
 		console.log("Telling the content script to begin gathering all inputs");
@@ -12,39 +12,40 @@ $(document).ready(() => {
 	});
 });
 
-const getData = (keys) => {
-	chrome.storage.sync.get(keys, (result) => {
-		//renderDefaultProfile(result);
-	});
-};
-
-const renderDefaultProfile = (result) => {
-	console.log(result);
-	let title = "";
-	let rows = "";
-	for (const url in result) {
-		title = `<h5 class='text-white text-center' id="${url}">` + url + "</h5>";
+const renderDefaultProfile = () => {
+	chrome.storage.sync.get("formData", (result) => {
+		let formData = result.formData;
+		console.log(formData);
 		rows = "";
-		for (const value in result[url]) {
-			rows +=
-				"<div class='input-group input-group-sm d-flex flex-row my-2' id=" +
-				value +
-				`><div type='text' class='form-control font-weight-bold input-left key' readonly>` +
-				value +
-				`</div><div readonly type='text' class='form-control input-right values'"/ aria-describedby='cancel'>` +
-				result[url][value] +
-				"</div><button class='input-group-append copy-button'>Copy to clipboard</button></div>";
+		for (const defaultTitle in formData) {
+			if (defaultTitle === "defaultProfile") {
+				for (const value in formData.defaultProfile) {
+					rows += `<tr>
+							<th scope='row'><i class="fas fa-clipboard"></i></th>
+							<td>${value}</td>
+							<td>${formData.defaultProfile[value]}</td>
+						</tr>`;
+				}
+			}
+			$(".container").append(
+				`<div class='rounded-sm py-5 px-3 data-container'>
+				<h5 class='text-primary text-center' id='${defaultTitle}'>${defaultTitle}</h5>
+					<table class='table table-striped'>
+						<thead>
+							<tr>
+								<th scope='col'>Copy</th>
+								<th scope='col'>Key</th>
+								<th scope='col'>Value</th>
+							</tr>
+						</thead>
+						<tbody>
+							${rows}
+						</tbody></table>
+					</div></div>`
+			);
 		}
-		$(".container").append(
-			"<div class='bg-info rounded-sm py-5 px-3 data-container'>" +
-				title +
-				"<div>" +
-				rows +
-				"</div>" +
-				"</div></div>"
-		);
-	}
-	bindCopy();
+		bindCopy();
+	});
 };
 
 const bindCopy = () => {
