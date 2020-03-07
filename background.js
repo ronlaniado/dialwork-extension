574146726c1e4212f0301e07e434341f300dc1eb
Ron <laniado.ron@gmail.com>
2020-03-07 14:20:39 -0500

    added default values

--- background.js
+++ background.js
@@ -1,7 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
+	const defaultProfile = {
+		name: "Ron",
+		email: "laniado.ron@gmail.com",
+		phone: "6464929374",
+		location: "Valley Stream",
+		zip: "11581"
+	};
 	console.log("Background script has been installed successfully.");
 	chrome.storage.sync.clear();
-	chrome.storage.sync.set({ formData: {} }, () => {
+	chrome.storage.sync.set({ formData: { defaultProfile } }, () => {
 		console.log("Form has been saved to storage");
 	});
 });
