17b3b76762a7463bdf0ea65e45428b405614c120
Ron <laniado.ron@gmail.com>
2020-03-12 13:45:36 -0400

    changed structure of popup

--- popup.js
+++ popup.js
@@ -1,15 +1,11 @@
 $(document).ready(() => {
-	chrome.runtime.sendMessage({ message: "keyRequest" }, (response) => {
-		//Sends a message to the background script telling it that options.html is active, and that we want the keys.
-		let keys = response.farewell;
-		renderDefaultProfile();
+	chrome.tabs.executeScript({
+		file: "contentScript.js"
 	});
-	$("#collectButton").on("click", () => {
-		console.log("Telling the content script to begin gathering all inputs");
-		chrome.tabs.executeScript({
-			file: "contentScript.js"
-		});
-	});
+	renderDefaultProfile();
+	bindCopy();
+	bindCollectInfo();
+	bindAutofill();
 });
 
 const renderDefaultProfile = () => {
@@ -21,15 +17,20 @@ const renderDefaultProfile = () => {
 			if (defaultTitle === "defaultProfile") {
 				for (const value in formData.defaultProfile) {
 					rows += `<tr>
-							<th scope='row'><i class="fas fa-clipboard"></i></th>
+							<th scope='row' class="copy-button">
+								<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="clipboard" class="svg-inline--fa fa-clipboard fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+									<path class="copy" d="M384 112v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h80c0-35.29 28.71-64 64-64s64 28.71 64 64h80c26.51 0 48 21.49 48 48zM192 40c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24m96 114v-20a6 6 0 0 0-6-6H102a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6h180a6 6 0 0 0 6-6z">
+									</path>
+								</svg>
+							</th>
 							<td>${value}</td>
-							<td>${formData.defaultProfile[value]}</td>
+							<td>${formData.defaultProfile[value]["value"]}</td>
 						</tr>`;
 				}
 			}
 			$(".container").append(
-				`<div class='rounded-sm py-5 px-3 data-container'>
-				<h5 class='text-primary text-center' id='${defaultTitle}'>${defaultTitle}</h5>
+				`<div class='rounded-sm py-2 px-3 data-container'>
+				<h5 class='text-info text-center' id='${defaultTitle}'>My default profile</h5>
 					<table class='table table-striped'>
 						<thead>
 							<tr>
@@ -44,7 +45,18 @@ const renderDefaultProfile = () => {
 					</div></div>`
 			);
 		}
-		bindCopy();
+	});
+};
+
+const bindCollectInfo = () => {
+	$("#collectButton").on("click", () => {
+		console.log("CLICKED");
+		// Send a message to the content script to begin collecting info
+		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, { message: "execCollectInfo" }, function(response) {
+				console.log(response.farewell);
+			});
+		});
 	});
 };
 
@@ -52,6 +64,25 @@ const bindCopy = () => {
 	console.log("binding copy!");
 	$(".copy-button").click((e) => {
 		console.log("button clicked!");
-		console.log($(e.target.previousSibling).text());
+		let copyText = $(e.target.parentNode.parentNode.nextSibling.nextSibling.nextElementSibling).text();
+		console.log(copyText);
+		let input = document.createElement("input");
+		input.value = copyText;
+		input.style.cssText = "opacity:0; position:fixed";
+		document.body.appendChild(input);
+		input.focus();
+		input.select();
+		document.execCommand("copy");
+		input.remove();
+	});
+};
+
+const bindAutofill = () => {
+	$("#autofillButton").click(() => {
+		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, { message: "execAutofill" }, function(response) {
+				console.log(response.farewell);
+			});
+		});
 	});
 };
