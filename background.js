5a4d8cef524af04bcb8543f5b6177ce66daa2840
Ron <laniado.ron@gmail.com>
2020-03-12 13:44:12 -0400

    changed default profile format

--- background.js
+++ background.js
@@ -1,27 +1,52 @@
 chrome.runtime.onInstalled.addListener(function() {
 	const defaultProfile = {
-		name: "Ron",
-		email: "laniado.ron@gmail.com",
-		phone: "6464929374",
-		location: "Valley Stream",
-		zip: "11581"
+		"First Name": {
+			value: "Bill",
+			autofill: "given-name"
+		},
+
+		"Last Name": {
+			value: "Gates",
+			autofill: "family-name"
+		},
+		Email: {
+			value: "bill.gates@microsoft.com",
+			autofill: "email"
+		},
+		Phone: {
+			value: "555-555-5555",
+			autofill: "tel"
+		},
+		Location: {
+			value: "Seattle",
+			autofill: ""
+		},
+		Zip: {
+			value: "11581",
+			autofill: "postal-code"
+		},
+		LinkedIn: {
+			value: "https://www.linkedin.com/in/williamhgates/",
+			autofill: "custom-question-linkedin-profile"
+		},
+		Website: {
+			value: "https://www.linkedin.com/in/williamhgates/",
+			autofill: "custom-question-webite"
+		}
 	};
 	console.log("Background script has been installed successfully.");
 	chrome.storage.sync.clear();
 	chrome.storage.sync.set({ formData: { defaultProfile } }, () => {
 		console.log("Form has been saved to storage");
 	});
-});
 
-let keys;
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	// Gets the keys to the data stored by the extension
-	if (request.message === "keyRequest") {
-		sendResponse({ farewell: keys });
-	} else if (Array.isArray(request.message)) {
-		keys = request.message;
-		console.log("keys: " + keys);
-		sendResponse({ farewell: "The url keys have been received" });
-	}
+	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+		// Gets the keys to the data stored by the extension
+		if (request.message === "execAutofill") {
+			chrome.tabs.executeScript({
+				file: "contentScript.js"
+			});
+			sendResponse({ farewell: "goodbye!" });
+		}
+	});
 });
