58166fc830c6bd8bcf88acc447c39a81ebc21256
Ron <laniado.ron@gmail.com>
2020-03-16 15:24:25 -0400

    put svg in button

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 		file: "contentScript.js"
 	});
 	renderDefaultProfile();
-	bindCopy();
+
 	bindCollectInfo();
 	bindAutofill();
 	bindOpenSettings();
@@ -18,11 +18,13 @@ const renderDefaultProfile = () => {
 			if (defaultTitle === "defaultProfile") {
 				for (const value in formData.defaultProfile) {
 					rows += `<tr>
-							<th scope='row' class="copy-button">
+							<th scope='row'>
+							<button class="copyButton">
 								<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="clipboard" class="svg-inline--fa fa-clipboard fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
 									<path class="copy" d="M384 112v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h80c0-35.29 28.71-64 64-64s64 28.71 64 64h80c26.51 0 48 21.49 48 48zM192 40c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24m96 114v-20a6 6 0 0 0-6-6H102a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6h180a6 6 0 0 0 6-6z">
 									</path>
 								</svg>
+								</button>
 							</th>
 							<td>${value}</td>
 							<td>${formData.defaultProfile[value]["value"]}</td>
@@ -63,7 +65,8 @@ const bindCollectInfo = () => {
 
 const bindCopy = () => {
 	console.log("binding copy!");
-	$(".copy-button").click((e) => {
+	console.log($(".copyButton"));
+	$(".copyButton").on("click", (e) => {
 		console.log("button clicked!");
 		let copyText = $(e.target.parentNode.parentNode.nextSibling.nextSibling.nextElementSibling).text();
 		console.log(copyText);
