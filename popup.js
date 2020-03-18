b9ad12344f21475da7e94f27836f0e36b3261736
Ron <laniado.ron@gmail.com>
2020-03-17 20:42:39 -0400

    implemented copy-to-clipboard

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ $(document).ready(() => {
 		file: "contentScript.js"
 	});
 	renderDefaultProfile();
-	bindCopy();
 	bindCollectInfo();
 	bindAutofill();
 	bindOpenSettings();
@@ -18,13 +17,11 @@ const renderDefaultProfile = () => {
 			if (defaultTitle === "defaultProfile") {
 				for (const value in formData.defaultProfile) {
 					rows += `<tr>
-							<th scope='row'>
-							<button class="copyButton">
+							<th scope='row' class="copyButton">
 								<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="clipboard" class="svg-inline--fa fa-clipboard fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
 									<path class="copy" d="M384 112v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h80c0-35.29 28.71-64 64-64s64 28.71 64 64h80c26.51 0 48 21.49 48 48zM192 40c-13.255 0-24 10.745-24 24s10.745 24 24 24 24-10.745 24-24-10.745-24-24-24m96 114v-20a6 6 0 0 0-6-6H102a6 6 0 0 0-6 6v20a6 6 0 0 0 6 6h180a6 6 0 0 0 6-6z">
 									</path>
 								</svg>
-								</button>
 							</th>
 							<td>${value}</td>
 							<td>${formData.defaultProfile[value]["value"]}</td>
@@ -48,6 +45,7 @@ const renderDefaultProfile = () => {
 					</div></div>`
 			);
 		}
+		bindCopy();
 	});
 };
 
